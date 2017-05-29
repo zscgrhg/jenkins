dd1862807ac9bc939634ceec28e49091000d20b0
zscgrhg <zscgrhg@qq.com>
2017-05-29 10:32:14 +0800

    hhh

---
+++ src/test/java/com/example/jenkins/TestUUUU.java
@@ -0,0 +1,18 @@
+package com.example.jenkins;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Created by think on 17-5-29.
+ */
+public class TestUUUU {
+    @Test
+    public void evaluatesExpression() {
+
+        int sum = 6;
+        assertEquals(6, sum);
+    }
+}
