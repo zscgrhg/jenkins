8b33177a9c236614fd0a82b73ea3f2f98e428833
zscgrhg <zscgrhg@qq.com>
2017-05-29 20:20:58 +0800

    hhh

--- src/main/java/com/example/jenkins/uuuu.java
+++ src/main/java/com/example/jenkins/uuuu.java
@@ -1,10 +1,17 @@
 package com.example.jenkins;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 /**
  * Created by think on 17-5-29.
  */
 public class uuuu {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("aaaa");
+        File f = new File("pom.xml");
+        FileInputStream fileInputStream = new FileInputStream(f);
+
     }
 }
