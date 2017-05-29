bf2bea31e224ad7739e0f1760bf47e780a1d7d32
zscgrhg <zscgrhg@qq.com>
2017-05-29 22:20:54 +0800

    hhh

--- src/main/java/com/example/hello/HelloA.java
+++ src/main/java/com/example/hello/HelloA.java
@@ -3,6 +3,9 @@ package com.example.hello;
 import com.example.OO.Oops;
 import com.example.jenkins.HelloB;
 
+import java.util.Random;
+import java.util.WeakHashMap;
+
 /**
  * Created by think on 17-5-29.
  */
@@ -12,5 +15,20 @@ public class HelloA {
 
     public static void main(String[] args) {
         System.out.println(new HelloB());
+    }
+
+    public static void t1() throws InterruptedException {
+        WeakHashMap<String, byte[][]> d = new WeakHashMap<String, byte[][]>();
+        for (int i = 0; i < 100; i++) {
+
+            int i1 = new Random().nextInt(10000);
+            String key = String.valueOf(i1);
+            d.put(key, new byte[100][100]);
+
+            //System.out.println(d.size());
+            System.gc();
+            Thread.sleep(100);
+        }
+        System.out.println(d.size());
     }
 }
--- src/main/java/com/example/jenkins/HelloB.java
+++ src/main/java/com/example/jenkins/HelloB.java
@@ -3,6 +3,9 @@ package com.example.jenkins;
 import com.example.OO.Oops;
 import com.example.hello.HelloA;
 
+import java.util.Random;
+import java.util.WeakHashMap;
+
 /**
  * Created by think on 17-5-29.
  */
@@ -12,5 +15,20 @@ public class HelloB {
 
     public static void main(String[] args) {
         System.out.println(new HelloA());
+    }
+
+    public static void t2() throws InterruptedException {
+        WeakHashMap<String, byte[][]> d = new WeakHashMap<String, byte[][]>();
+        for (int i = 0; i < 100; i++) {
+
+            int i1 = new Random().nextInt(10000);
+            String key = String.valueOf(i1);
+            d.put(key, new byte[100][100]);
+
+            //System.out.println(d.size());
+            System.gc();
+            Thread.sleep(100);
+        }
+        System.out.println(d.size());
     }
 }
