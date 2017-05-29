db64b67d433f715c331485f943dc0fc6178ffd37
zscgrhg <zscgrhg@qq.com>
2017-05-29 16:03:14 +0800

    hhh

--- src/main/java/com/example/jenkins/TPipeline.java
+++ src/main/java/com/example/jenkins/TPipeline.java
@@ -4,6 +4,9 @@ package com.example.jenkins;
  * Created by think on 17-5-28.
  */
 public class TPipeline {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("hello jenkins");
     }
