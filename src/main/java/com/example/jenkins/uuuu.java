b693af6cf9c240bee751d4ad2f01d44901eb2d58
zscgrhg <zscgrhg@qq.com>
2017-05-29 10:11:54 +0800

    hhh

---
+++ src/main/java/com/example/jenkins/uuuu.java
@@ -0,0 +1,10 @@
+package com.example.jenkins;
+
+/**
+ * Created by think on 17-5-29.
+ */
+public class uuuu {
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
+}
